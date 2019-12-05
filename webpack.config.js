4fbc78f54d7dafa5c34d8e3146c63529a02ad4f7
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2019-12-05 14:02:19 -0500

    inicio version 2 app parners

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,51 @@
-const webpack = require('webpack');
-const dotenv = require('dotenv');
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtraxtPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtraxtPlugin = require('mini-css-extract-plugin')
 
-module.exports = () => {
-  const env = dotenv.config().parsed;
-
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-
-  return ({
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/',
+module.exports = {
+    entry : './src/index.js',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
     },
-    resolve: {
-      extensions: ['.js', '.jsx'],
+    resolve:{
+        extensions: ['.js', '.jsx']
     },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
-        },
-        {
-          test: /\.html$/,
-          use: {
-            loader: 'html-loader',
-          },
-        },
-        {
-          test: /\.(s*)css$/,
-          exclude: /node_modules/,
-          use: [{
-            loader: MiniCssExtraxtPlugin.loader,
-          },
-          'css-loader',
-          'sass-loader',
-          ],
-        },
-        {
-          test: /\.(png|gif|jpg|svg)$/,
-          use: [{
-            loader: 'file-loader',
-            options: {
-              name: 'assets/[name].¨[ext]',
+    module:{
+        rules:[
+            {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+                }
             },
-          }],
-        },
-      ],
+            {
+                test:/\.html$/,
+                use: {
+                    loader: 'html-loader'
+                    }
+            },
+            {
+                test: /\.(s*)css$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: MiniCssExtraxtPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
     },
-    devServer: {
-      contentBase: path.join(__dirname, './dist'),
-      compress: true,
-      historyApiFallback: true,
-    },
-    node: {
-      fs: 'empty',
-    },
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: './public/index.html',
-        filename: './index.html',
-      }),
-      new MiniCssExtraxtPlugin({
-        filename: 'assets/[name].css',
-      }),
-      new webpack.DefinePlugin(envKeys),
-    ],
-  });
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtraxtPlugin({
+            filename: 'assets/[name].css'
+        }),
+    ]
+
 };
