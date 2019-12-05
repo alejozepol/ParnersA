0f19d4f91bf82bc90805ef73e2a45eddca0e0384
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2019-12-05 14:22:45 -0500

    archivo de configuracion constantes firebase utilizando dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,61 @@
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin'); 
-const MiniCssExtraxtPlugin = require('mini-css-extract-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtraxtPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry : './src/index.js',
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js'
-    },
-    resolve:{
-        extensions: ['.js', '.jsx']
-    },
-    module:{
-        rules:[
-            {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-                }
-            },
-            {
-                test:/\.html$/,
-                use: {
-                    loader: 'html-loader'
-                    }
-            },
-            {
-                test: /\.(s*)css$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: MiniCssExtraxtPlugin.loader,
-                    },
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-        ]
-    },
-    plugins:[
-        new HtmlWebPackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtraxtPlugin({
-            filename: 'assets/[name].css'
-        }),
-    ]
+module.exports = () => {
+  const env = dotenv.config().parsed;
 
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+  return ({
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: 'html-loader',
+          },
+        },
+        {
+          test: /\.(s*)css$/,
+          exclude: /node_modules/,
+          use: [{
+            loader: MiniCssExtraxtPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+      }),
+      new MiniCssExtraxtPlugin({
+        filename: 'assets/[name].css',
+      }),
+      new webpack.DefinePlugin(envKeys),
+    ],
+  });
 };
