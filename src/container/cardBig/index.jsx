a6a0b18ef219e5b142ddf3d08cc67f504b38e1e6
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2019-12-05 22:57:49 -0500

    cardBig

---
+++ src/container/cardBig/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CardBig.scss';
+
+const CardBig = ({ children }) => (
+  <section className='CardBig'>
+    {children}
+  </section>
+);
+
+export default CardBig;
