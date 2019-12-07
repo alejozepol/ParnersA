73561928d61b24eeb91f855061aa4f2824d044f2
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2019-12-07 00:12:42 -0500

    suscripcion

--- src/pages/Evento.jsx
+++ src/pages/Evento.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { FirebaseApp } from '../services/firebase/index';
 import CardBig from '../container/cardBig';
+import Button from '../components/button';
 import portada from '../assets/static/football_detalle.png';
 import Ubicacion from '../assets/icons/Ubicacion.png';
 import Participantes from '../assets/icons/Participantes.png';
@@ -11,16 +12,55 @@ const evento = (props) => {
   const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
   const { id } = props.match.params;
   const colectionEventos = FirebaseApp.firestore().collection('eventos');
-  const [eventoDB, setEventoDB] = useState([]);
+  const colectionEventoUsuario = FirebaseApp.firestore().collection('eventoUsuario');
+  const { email } = FirebaseApp.auth().currentUser;
+  const [eventoDB, setEventoDB] = useState({
+    isTrue: false,
+    email,
+    idEvento: id,
+  });
+  const [eventoUsuarioBD, setEventoUsuarioBD] = useState([]);
+
   useEffect(() => {
     const Evento = colectionEventos.doc(id).get()
       .then((data) => setEventoDB(data.data()))
       .catch((e) => console.log(e));
   }, []);
+  useEffect(() => {
+    email === 'undefined' && 'a@a.com';
+    const EventoUsuario = colectionEventoUsuario.doc(email).get()
+      .then((data) => setEventoUsuarioBD(data.data()))
+      .catch((e) => console.log(e));
+  }, []);
+
+  function suscripcion() {
+    let info = eventoUsuarioBD;
+    if (eventoUsuarioBD) {
+      info.isTrue ? info.isTrue = false : info.isTrue = true;
+    } else {
+      info = {
+        isTrue: true,
+        email,
+        idEvento: id,
+      };
+    }
+    colectionEventoUsuario
+      .doc(email)
+      .set(info)
+      .then()
+      .catch((error) => error);
+  }
   return (
     <section className='Evento'>
       <CardBig>
-        <img src={portada} alt={portada} />
+        <div>
+          <img src={portada} alt={portada} />
+          {eventoUsuarioBD.isTrue ? (
+            <button type='button' className='unsuscrited' onClick={() => suscripcion()}>No Aistire</button>
+          ) : (
+            <button type='button' className='suscribir' onClick={() => suscripcion()}>Inscribirme</button>
+          )}
+        </div>
         <div className='Evento_content'>
           <div>
             <h3>{eventoDB.TITULO}</h3>
@@ -49,7 +89,7 @@ const evento = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-
+    ...state,
   };
 
 };
