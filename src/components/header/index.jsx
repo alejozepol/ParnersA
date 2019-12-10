6cc672e82b70bd539ef15f7f5f1efb03b752ad31
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2019-12-10 01:26:58 -0500

    arreglo estilos navegacion

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Icons from '../icons';
 import Logo from '../../assets/static/Logo.png';
 import IsoTipo from '../../assets/static/isoTipo.png';
@@ -8,12 +9,14 @@ const Header = ({ dir, title }) => {
   return (
     <section className='Header'>
       <div className='Header__atras'>
-        {dir !== '/' && <Icons type='atras' />}
+        {dir !== '/' && (
+          <Link to='/' className='Footer__perfil'>
+            <Icons type='atras' />
+          </Link>
+        )}
       </div>
       <div className='Header__logo'>
         {dir === '/' && <img src={Logo} alt='Logo Parners' />}
-      </div>
-      <div className='Header__title'>
         {dir !== '/' && <h2>{String(dir).slice(1).toLocaleUpperCase()}</h2>}
       </div>
       <div className='Header__isotipo'>
--- src/pages/Personas.jsx
+++ src/pages/Personas.jsx
@@ -11,6 +11,7 @@ const Personas = (props) => {
   const colectionPersonas = FirebaseApp.firestore().collection('user');
   const [personasDB, setPersonasDB] = useState([]);
   useEffect(() => {
+    // eslint-disable-next-line no-unused-vars
     const listEvento = colectionPersonas.onSnapshot(({ docs }) => {
       const personasFromDB = [];
       docs.forEach((doc) => {
@@ -31,7 +32,7 @@ const Personas = (props) => {
         <Card img={e.photoURL} key={e.id}>
           <div className='Personas_content'>
             <Link to={`persona/${e.EMAIL}`}>
-              <h3>{e.name}</h3>
+              <h3>{`${String(e.name).slice(0, 15)}...`}</h3>
               <h4>
                 <img src={Ubicacion} alt='Ubicacion' />
                 {e.UBICACION ? (
